a0e5e9c99c1ee84a99f7009616fd4cc56c5f19e8
droptableifexists <ashitmapants637@gmail.com>
2025-05-07 16:01:25 -0400

    Refine SQL query in getConstraints function to utilize text representation for OID comparisons, ensuring accurate constraint retrieval.

--- get_full_schema.go
+++ get_full_schema.go
@@ -190,8 +190,8 @@ func getConstraints(db *sql.DB, schema string, table string) ([]ConstraintSchema
 	FROM
 		pg_constraint
 	WHERE
-		conrelid = ($1::text || '.' || $2::text)::regclass AND
-		connamespace = $3::regnamespace`, schema, table, schema)
+		conrelid::regclass::text = $1 AND
+		connamespace::regnamespace::text = $2`, table, schema)
 	if err != nil {
 		return nil, err
 	}
