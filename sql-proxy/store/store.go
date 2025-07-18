e1f24b5221d6d1466bf1f72ff2e76a0c04ab3f6b
droptableifexists <ashitmapants637@gmail.com>
2025-07-18 11:24:00 -0400

    do not store duplicate queries

--- sql-proxy/store/store.go
+++ sql-proxy/store/store.go
@@ -1,21 +1,35 @@
 package store
 
+import "sync"
+
 type QueryExecuted struct {
 	Query string
 }
 
 type QueryStore struct {
-	queryMap []QueryExecuted
+	queryMap map[string]QueryExecuted
+	mu       sync.RWMutex
 }
 
 func MakeQueryStore() *QueryStore {
-	return &QueryStore{}
+	return &QueryStore{
+		queryMap: make(map[string]QueryExecuted),
+	}
 }
 
 func (qs *QueryStore) AddQuery(q QueryExecuted) {
-	qs.queryMap = append(qs.queryMap, q)
+	qs.mu.Lock()
+	defer qs.mu.Unlock()
+	qs.queryMap[q.Query] = q
 }
 
-func (qs QueryStore) ListQueries() []QueryExecuted {
-	return qs.queryMap
+func (qs *QueryStore) ListQueries() []QueryExecuted {
+	qs.mu.RLock()
+	defer qs.mu.RUnlock()
+
+	queries := make([]QueryExecuted, 0, len(qs.queryMap))
+	for _, query := range qs.queryMap {
+		queries = append(queries, query)
+	}
+	return queries
 }
